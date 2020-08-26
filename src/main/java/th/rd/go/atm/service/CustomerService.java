ed3bdf90c1b4c16af883b7e93f1c8dff3e3058bf
athittaya.ch <athittaya.ch@gmail.com>
2020-08-26 15:21:41 +0700

    Add Login & Hash

--- src/main/java/th/rd/go/atm/service/CustomerService.java
+++ src/main/java/th/rd/go/atm/service/CustomerService.java
@@ -1,6 +1,6 @@
 package th.rd.go.atm.service;
 
-//import org.mindrot.jbcrypt.BCrypt;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.rd.go.atm.model.Customer;
 
@@ -8,57 +8,58 @@ import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 @Service
 public class CustomerService {
 
-    private List<Customer> customerList;
+    //private List<Customer> customerList;
+    private ArrayList<Customer> customers = new ArrayList<>();
 
-    @PostConstruct
-    public void postConstruct() {
-        this.customerList = new ArrayList<>();
-    }
 
-    public void createCustomer(Customer customer) {
-        customerList.add(customer);
-    }
-
-//    public void createCustomer(Customer customer) {
-//        String hashPin = hash(customer.getPin());
-//        customer.setPin(hashPin);
-//        customers.add(customer);
+//    @PostConstruct
+//    public void postConstruct() {
+//        this.customerList = new ArrayList<>();
 //    }
 
+
+    public void createCustomer(Customer customer) {
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
+        customers.add(customer);
+    }
 
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(this.customerList);
+        return new ArrayList<>(customers);
     }
-//
-//    private String hash(String pin) {
-//        String salt = BCrypt.gensalt(12);
-//        return BCrypt.hashpw(pin, salt);
-//    }
 
-//
-//    public Customer checkPin(Customer inputCustomer) {
-//        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
-//        Customer matchingCustomer = null;
-//        for (Customer c : customers) {
-//            if (inputCustomer.getId() == c.getId())
-//                matchingCustomer = c;
-//        }
-//        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
-//        if (matchingCustomer != null) {
-//            if (BCrypt.checkpw(inputCustomer.getPin(),
-//                    matchingCustomer.getPin()))
-//                return matchingCustomer;
-//        }
-//        // 3. ถ้าไม่ตรง ต้องคืนค่า null
-//        return null;
-//    }
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
+    }
+
 
     public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer matchingCustomer = null;
+        for (Customer c : customers) {
+            if (inputCustomer.getId() == c.getId())
+                matchingCustomer = c;
+        }
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (matchingCustomer != null) {
+            if (BCrypt.checkpw(inputCustomer.getPin(),
+                    matchingCustomer.getPin()))
+                return matchingCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
         return null;
     }
+
+//    public Customer checkPin(Customer inputCustomer) {
+//        return null;
+//    }
 
 }
